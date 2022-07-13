de88654c4d24f3c9460d672a267b811b4222628b
Dianliang233 <dianliang233@gmail.com>
2022-07-13 21:23:11 +0800

    Add Prisma schema

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     CredentialsProvider({
